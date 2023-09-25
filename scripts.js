bd0bf490ab5c39ba15d4bdf80085a44328db0583
worsleyj <josephworsley23@gmail.com>
2023-09-25 13:29:50 -0400

    finished implementation of rock paper scissors

--- scripts.js
+++ scripts.js
@@ -1,24 +1,54 @@
 // console.log("test")
 
+let playerScore = 0;
+let computerScore = 0;
+let gameLength = 0; // default game length, dynamically set via prompt
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let computerChoice = choices[Math.floor(Math.random()*3)]
-    // console.log("Computer has chosen " + computerChoice); 
+    console.log("Computer has chosen " + computerChoice + "."); 
     return computerChoice;
 }
 
 function singleRound(playerChoice, computerChoice) {
-    if ((playerChoice == "Rock" && computerChoice == "Scissors")
+    playerChoice = playerChoice.substring(0,1).toUpperCase() + playerChoice.substring(1).toLowerCase(); // capitalizes first letter only
+    
+    if ((playerChoice == "Rock" && computerChoice == "Scissors") // winning case
     ||(playerChoice == "Scissors" && computerChoice == "Paper")
     ||(playerChoice == "Paper" && computerChoice == "Rock")) {
         console.log("You win! " + playerChoice + " beats " + computerChoice + ".");
-    } else if ((playerChoice == "Scissors" && computerChoice == "Rock")
+        playerScore++;
+    } else if ((playerChoice == "Scissors" && computerChoice == "Rock") // losing case
     ||(playerChoice == "Paper" && computerChoice == "Scissors")
     ||(playerChoice == "Rock" && computerChoice == "Paper")) {
         console.log("You lose! " + computerChoice + " beats " + playerChoice + ".");
-    } else {
+        computerScore++;
+    } else if (playerChoice == computerChoice) { // draw case
         console.log("It's a draw! You both picked " + playerChoice + "!")
+    } else { // otherwise, input is invalid
+        console.log("Invalid Input!")
+        console.log("You Entered: " + playerChoice)
     }
 }
 
-// singleRound("Rock", getComputerChoice())
+function game() {
+    let counter = 0;
+    gameLength = parseInt(prompt("How many rounds would you like to play? Whole numbers only"))
+    while (counter < gameLength) {
+        singleRound(prompt("Choose Rock, Paper, or Scissors"), getComputerChoice())
+        counter++;
+    }
+
+    console.log("You won " + playerScore + " rounds out of " + gameLength + ", while the computer won " + computerScore + " rounds.")
+    console.log(gameLength-(playerScore+computerScore) + " rounds ended in a draw.")
+    if (playerScore > computerScore) {
+        console.log("You won the game! To play again, refresh the page.")
+    } else if (playerScore < computerScore) {
+        console.log("You lost the game! To play again, refresh the page.")
+    } else {
+        console.log("This game was a draw! To play again, refresh the page.")
+    }
+}
+
+game()
